b58bf3d8f2fe09640bd20d00e67021f09b8bfd49
coddey-me <113989495+coddey-me@users.noreply.github.com>
2025-09-29 16:26:25 +0300

    Update config.py

--- model/config.py
+++ model/config.py
@@ -8,7 +8,7 @@ class AkiaConfig:
     vocab_size = 50257  # GPT-2 tokenizer
     max_seq_length = 2048
     embedding_dim = 384
-    hidden_dim = 512
+    hidden_dim = 504
     num_hierarchy_levels = 4
     num_attention_heads = 6
     ffn_multiplier = 2
