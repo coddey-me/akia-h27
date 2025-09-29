321d521bb3a262af674fd686f0f87e772f8679c3
coddey-me <113989495+coddey-me@users.noreply.github.com>
2025-09-29 16:47:20 +0300

    Update train.py, line 128 class

--- training/train.py
+++ training/train.py
@@ -126,8 +126,8 @@ def main():
     
     # Wrap with DDP if using multi-GPU
     if args.use_ddp:
-        model = model.to(device)
-        model = DDP(model, device_ids=[local_rank])
+        model = model.to(local_rank)
+        model = DDP(model, device_ids=[local_rank], output_device=local_rank, find_unused_parameters=True)
     else:
         model = model.to(device)
     
