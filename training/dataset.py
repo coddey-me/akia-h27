c53365a2468261606f4dc24e6203aa9423eeb99f
coddey-me <113989495+coddey-me@users.noreply.github.com>
2025-09-29 15:57:27 +0300

    Create dataset.py

---
+++ training/dataset.py
@@ -0,0 +1,88 @@
+"""
+Dataset loader for Akia HRM training
+"""
+
+import torch
+from torch.utils.data import Dataset
+import pickle
+
+
+class AkiaHRMDataset(Dataset):
+    """Dataset for hierarchical reasoning training"""
+    
+    def __init__(self, pkl_file, tokenizer, max_length=2048):
+        with open(pkl_file, 'rb') as f:
+            self.data = pickle.load(f)
+        
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        item = self.data[idx]
+        
+        # Construct hierarchical input
+        low_ctx = item.get('low_level_context', '')
+        high_ctx = item.get('high_level_context', '')
+        input_seq = item.get('input_sequence', '')
+        target = item.get('target_sequence', '')
+        
+        # Format: [LOW: ...] [HIGH: ...] [INPUT: ...] [TARGET: ...]
+        full_input = f"[LOW: {low_ctx}] [HIGH: {high_ctx}] [INPUT: {input_seq}] [TARGET: {target}]"
+        
+        # Tokenize
+        encoding = self.tokenizer(
+            full_input,
+            max_length=self.max_length,
+            truncation=True,
+            padding='max_length',
+            return_tensors='pt'
+        )
+        
+        input_ids = encoding['input_ids'].squeeze(0)
+        attention_mask = encoding['attention_mask'].squeeze(0)
+        
+        # Labels are same as input_ids for causal LM
+        labels = input_ids.clone()
+        
+        # Mask padding tokens in labels
+        labels[labels == self.tokenizer.pad_token_id] = -100
+        
+        return {
+            'input_ids': input_ids,
+            'attention_mask': attention_mask,
+            'labels': labels,
+            'metadata': {
+                'id': item.get('id', ''),
+                'domain': item.get('domain', ''),
+                'difficulty': item.get('difficulty', ''),
+                'quality_score': item.get('quality_score', 0.0)
+            }
+        }
+
+
+def create_dataloaders(train_pkl, val_pkl, tokenizer, batch_size=16, num_workers=4):
+    """Create train and validation dataloaders"""
+    
+    train_dataset = AkiaHRMDataset(train_pkl, tokenizer)
+    val_dataset = AkiaHRMDataset(val_pkl, tokenizer)
+    
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        pin_memory=True
+    )
+    
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=num_workers,
+        pin_memory=True
+    )
+    
+    return train_loader, val_loader
